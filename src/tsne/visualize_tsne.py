67d87d480869af4324724b3b8fd4e7bc40070c0e
lukerm <13883308+lukerm@users.noreply.github.com>
2025-06-22 07:07:45 +0100

    improve t-SNE study

--- src/tsne/visualize_tsne.py
+++ src/tsne/visualize_tsne.py
@@ -1,7 +1,12 @@
 import os
 
+import matplotlib; matplotlib.get_backend(); matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
 import numpy as np
-from dotenv import load_dotenv
+import openai
+import pandas as pd
+import seaborn as sns
+from dotenv import load_dotenv; load_dotenv()
 from sklearn.manifold import TSNE
 
 from ..database.weaviate_client import XKCDWeaviateClient
@@ -29,7 +34,7 @@ results = results.get("data", {}).get("Get", {}).get("XKCDComic", [])
 
 
 
-
+# Build the matrix of embedding vectors
 X = np.zeros(shape=(3_000, LATENT_DIM_SIZE))
 ids, titles = [], []
 for i, res in enumerate(results):
@@ -37,10 +42,10 @@ for i, res in enumerate(results):
     ids.append(res['comic_id'])
     titles.append(res['title'])
 
-import openai
-from dotenv import load_dotenv; load_dotenv()
+
+# Manually embed various query terms
+queries = ["barrel", "centripetal", "computer programming", "rocket", "USB", "vehicles"]
 client_openai = openai.OpenAI(api_key=os.environ['OPENAI_API_KEY'])
-queries = ["automobile", "centripetal", "correlation", "flying", "programming", "USB"]
 Q = np.zeros(shape=(len(queries), LATENT_DIM_SIZE))
 for i, q in enumerate(queries):
     response = client_openai.embeddings.create(
@@ -49,49 +54,41 @@ for i, q in enumerate(queries):
     )
     Q[i, :] = response.data[0].embedding
 
-# Combine data sets
+# Combine / append data sets together (queries last)
 XQ = np.concatenate([X, Q], axis=0)
 ids.extend([-1]*len(Q))
 titles.extend(queries)
 
 
-tsne = TSNE(n_components=2, random_state=2025)
-XQ_reduced = tsne.fit_transform(XQ)
-print(XQ_reduced.shape)
-
-
-
+# Perform ANN on our vector DB for each query
 limit = 10
 comic_groups = {}
 for q in queries:
-    retrieved = search_comics(client=cli, query=q, limit=limit, alpha=0.5)
+    retrieved = search_comics(client=cli, query=q, limit=limit, alpha=0.5, max_id=max_id)
     for comic in retrieved:
         comic_groups[comic['comic_id']] = q
 
 
+# t-SNE for dimensionality reduction
+tsne = TSNE(n_components=2, random_state=2025, perplexity=20)  # or 25
+XQ_reduced = tsne.fit_transform(XQ)
+print(XQ_reduced.shape)
 
-import matplotlib; matplotlib.get_backend(); matplotlib.use('Qt5Agg')
-import pandas as pd
-import seaborn as sns
-
-
-
-
-
+# Create a DataFrame with post-transform 2D coordinates
 df_tsne = pd.DataFrame(XQ_reduced, columns = ['dim1', 'dim2'])
-
-plot_labels = [comic_groups.get(comic_id, 'Other') for comic_id in ids[:-len(queries)]] + titles[-len(queries):]
+plot_labels = [comic_groups.get(comic_id, 'other') for comic_id in ids[:-len(queries)]] + titles[-len(queries):]
 alphas = [0.2 if label == 'Other' else 0.9 for label in plot_labels]
-df_tsne['Category'] = plot_labels
-df_tsne['Title'] = titles
+df_tsne['category'] = plot_labels
+df_tsne['title'] = titles
+df_tsne['comic_id'] = ids
 
+save_file = os.path.expanduser(os.path.join("~", "xkcd-comic-finder", "data", "df_tsne.csv"))
+df_tsne.to_csv(save_file, index=False)
 
+# Visualize scatter plot
 ax = sns.scatterplot(x='dim1', y='dim2', data=df_tsne,
-                    hue='Category', #hue_order=plot_categories + ['Other'],
-                    # alpha=alphas)
-                    alpha=0.75)
-
+                    hue='category',
+                    alpha=0.75)  # alpha=alphas
 # Label specific query points
 for i, row in df_tsne.tail(len(queries)).iterrows():
-    ax.annotate(f'Q:{row["Title"]}', (row['dim1'], row['dim2']), xytext=(5, 5), textcoords='offset points')
-    # ax.annotate(text=f'foo', xy=(row['dim1'], row['dim2']), xytext=(5, 5), textcoords='offset points')
+    ax.annotate(f'Q:{row["title"]}', (row['dim1'], row['dim2']), xytext=(5, 5), textcoords='offset points')
